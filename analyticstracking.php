1ec507e21b39bd3486c89cfc953dfc5aebd69326
harlworkshop <m_solis_cid@hotmail.com>
2021-05-25 13:16:44 -0400

    from php to html

--- analyticstracking.php
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-WN9STG8RZ2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-WN9STG8RZ2');
-</script>
--- program.php
+++
@@ -1,3 +0,0 @@
-<div class="container" style="font-size:18px;">
-	To be announced
-</div>
